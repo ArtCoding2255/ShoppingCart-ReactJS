30512570a085088aa299d7beec3db51f67692a0f
Katepiss Sriburapa <artcoding2544@gmail.com>
2024-03-06 20:50:10 +0700

    feat: set router for menus and set mock data

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Cart from './components/Cart';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import About from './pages/About';
+import Store from './pages/Store';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/cart" Component={Cart} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,28 @@ import {
   FormControl,
   Dropdown,
   Badge,
+  Nav,
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>Shopping Cart</Navbar.Brand>
+        <Nav>
+          <Navbar.Brand>Shopping Cart</Navbar.Brand>
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+
         <Navbar.Text className="search">
           <FormControl style={{ width: 500 }} className="m-auto" />
         </Navbar.Text>
@@ -22,7 +35,6 @@ const Header = () => {
             <FaShoppingCart color="white" fontSize="25px" />
             <Badge bg="transpalent">10</Badge>
           </Dropdown.Toggle>
-
           <Dropdown.Menu style={{ minWidth: 370 }}>
             <span style={{ padding: 10 }}>Cart is empty</span>
           </Dropdown.Menu>
