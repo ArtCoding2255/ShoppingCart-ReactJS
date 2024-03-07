a8ca3f43eacdf778a9f8369b28e34b847bcd4631
Katepiss Sriburapa <artcoding2544@gmail.com>
2024-03-07 07:46:04 +0700

    feat: create StoreItem for each item in the store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import storeItem from '../data/items.json';
+import StoreItem from '../components/StoreItem';
 
 function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItem.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Store;
